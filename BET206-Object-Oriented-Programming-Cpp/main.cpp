a4ff52d05df352f5e6e5429aaf4fc1688e0e1f6d
Addae Selom Caleb <calebaddae610@gmail.com>
2026-06-18 18:03:57 +0000

    Create main.cpp

---
+++ BET206-Object-Oriented-Programming-Cpp/main.cpp
@@ -0,0 +1,16 @@
+cpp
+#include <iostream>
+using namespace namespace std;
+int main () {
+int a, b;
+int s, D, m, q;
+cout « "'Sum ="«<a+b«
+"\n";
+cout << "Difference = " « a -
+b« "\n";
+cout < "Product = " « a * b
+«"\n"';
+cout « "Quotient =" « a / b
+« "\n";
+return 0;
+}
